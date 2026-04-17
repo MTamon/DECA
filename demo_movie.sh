2db1c4c04e0d8fb5dba3984e93bf0106f49305e2
MTamon <mtamon.0044@gmail.com>
2026-04-17 15:05:47 +0900

    [fix] .gitignore & demo_movie.sh & [add] demo extract_video_params.py

--- demo_movie.sh
+++ demo_movie.sh
@@ -1,3 +1,5 @@
 # python demos/demo_reconstruct.py -i TestSamples/movie/movie_demo.mp4 --sample_step 1 --rasterizer_type=standard
 
-python demos/demo_webcam2.py --source TestSamples/movie/movie_demo.mp4 --device cuda --rasterizer_type standard
+# python demos/demo_webcam2.py --source TestSamples/movie/movie_demo.mp4 --device cuda --rasterizer_type standard
+
+python demos/demo_webcam2.py --source 0 --device cuda --rasterizer_type standard
